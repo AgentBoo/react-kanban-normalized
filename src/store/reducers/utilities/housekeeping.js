c09dd7c3e7bd918606abbfadc38942b984ca6037
AgentBoo <zuzi.han@gmail.com>
2018-04-07 23:01:48 -0400

    Updates

--- src/store/reducers/utilities/housekeeping.js
+++
@@ -1,8 +0,0 @@
-// ============================================================================ //
-// UTILITY FUNCTIONS
-// ============================================================================ //
-// Encapsulate Object.assign
-export const updateObject = (current, updates) => Object.assign({}, current, updates)
-
-// Encapsulate updating an array
-export const updateArray = (current, updates) => [...current, updates]
